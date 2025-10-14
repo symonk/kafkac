9e9b43a6fbc711a5486eaa99ffc2b42b36a486cb
symonk <jackofspaces@gmail.com>
2025-10-14 16:23:50 +0100

    add `start()`

--- src/kafkac/consumer.py
+++ src/kafkac/consumer.py
@@ -9,14 +9,20 @@ class KafkaConsumer:
     def __init__(
         self,
         topics: tuple[str, ...],
-        librdkafka_settings: dict[str, typing.Any],
+        librdkafka_config: dict[str, typing.Any],
     ) -> None:
-        self.consumer = Consumer()
+        self.consumer = Consumer(**librdkafka_config)
+
+    def start(self) -> None:
+        """start signals the consumer to actually begin.  This is implicit
+        when KafkaConsumer is used as a context manager."""
 
     def __enter__(self) -> typing.Self:
         """__enter__ allows the KafkaConsumer instance to be used as a context
         manager, guaranteeing its graceful exit and teardown."""
+        self.start()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.consumer.close()
         return None
